6c4a186770f32575bc62ab4fc04bf179f1690275
Henrique Muniz <112440064+HenriMLT@users.noreply.github.com>
2023-09-22 19:23:35 -0300

    Create banco_de_dados.php

---
+++ banco_de_dados.php
@@ -0,0 +1,17 @@
+<?php
+// Banco de Dados
+$bancoDeDados = [
+    1 => [
+        'nome' => 'João',
+        'email' => 'joao@email.com',
+    ],
+    2 => [
+        'nome' => 'Maria',
+        'email' => 'maria@email.com',
+    ],
+    3 => [
+        'nome' => 'Pedro',
+        'email' => 'pedro@email.com',
+    ],
+];
+?>
